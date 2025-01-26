8d5ff406b62e78a887225d2faa8d6ef8939654bf
Federico Barberon <jfedericobarberonj@gmail.com>
2025-01-26 18:55:55 -0300

    refactor: add some tests helper functions

--- 2-url-shortener/handler_test.go
+++ 2-url-shortener/handler_test.go
@@ -10,13 +10,7 @@ import (
 )
 
 func TestMapHandler(t *testing.T) {
-	fallback := http.NewServeMux()
-	fallback.HandleFunc("/notMapped", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "test")
-	})
-	fallback.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "default mux")
-	})
+	fallback := fallbackMux()
 
 	paths := map[string]string{
 		"/hello":     "/bye",
@@ -54,43 +48,63 @@ func TestMapHandler(t *testing.T) {
 
 	for _, testCase := range redirectCases {
 		t.Run(fmt.Sprintf("redirects %s to %s", testCase.path, testCase.redirectTo), func(t *testing.T) {
-			request, _ := http.NewRequest(http.MethodGet, testCase.path, nil)
-			response := httptest.NewRecorder()
-
-			handler := urlshort.MapHandler(paths, fallback)
-
-			handler.ServeHTTP(response, request)
-
-			statusGot := response.Code
-			statusWanted := http.StatusSeeOther
-
-			if statusGot != statusWanted {
-				t.Errorf("expected status %d but got %d", statusWanted, statusGot)
-			}
-
-			urlObj, _ := response.Result().Location()
-			urlGot := urlObj.String()
-
-			if urlGot != testCase.redirectTo {
-				t.Errorf("expected redirect to %s but got %s", testCase.redirectTo, urlGot)
-			}
+			assertRedirect(t, urlshort.MapHandler(paths, fallback), testCase.path, testCase.redirectTo)
 		})
 	}
 
 	for _, testCase := range noRedirectCases {
 		t.Run("use fallback on "+testCase.path, func(t *testing.T) {
-			request, _ := http.NewRequest(http.MethodGet, testCase.path, nil)
-			response := httptest.NewRecorder()
-
-			handler := urlshort.MapHandler(paths, fallback)
-
-			handler.ServeHTTP(response, request)
-
-			got := response.Body.String()
-
-			if got != testCase.bodyStr {
-				t.Errorf("expected %s but got %s", testCase.bodyStr, got)
-			}
+			assertBody(t, urlshort.MapHandler(paths, fallback), testCase.path, testCase.bodyStr)
 		})
+	}
+}
+
+func fallbackMux() http.Handler {
+	fallback := http.NewServeMux()
+	fallback.HandleFunc("/notMapped", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "test")
+	})
+	fallback.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "default mux")
+	})
+
+	return fallback
+}
+
+func assertRedirect(t testing.TB, handler http.Handler, path, redirectTo string) {
+	t.Helper()
+
+	request, _ := http.NewRequest(http.MethodGet, path, nil)
+	response := httptest.NewRecorder()
+
+	handler.ServeHTTP(response, request)
+
+	statusGot := response.Code
+	statusWanted := http.StatusSeeOther
+
+	if statusGot != statusWanted {
+		t.Errorf("expected status %d but got %d", statusWanted, statusGot)
+	}
+
+	urlObj, _ := response.Result().Location()
+	urlGot := urlObj.String()
+
+	if urlGot != redirectTo {
+		t.Errorf("expected redirect to %s but got %s", redirectTo, urlGot)
+	}
+}
+
+func assertBody(t testing.TB, handler http.Handler, path, bodyStr string) {
+	t.Helper()
+
+	request, _ := http.NewRequest(http.MethodGet, path, nil)
+	response := httptest.NewRecorder()
+
+	handler.ServeHTTP(response, request)
+
+	got := response.Body.String()
+
+	if got != bodyStr {
+		t.Errorf("expected %s but got %s", bodyStr, got)
 	}
 }
